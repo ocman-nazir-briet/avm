c8313eb846d62f987749a7b0e3b56c4714020413
Ocman Nazir Briet <49003689+ocman-nazir-briet@users.noreply.github.com>
2022-08-01 21:39:31 +0500

    Update settings.py

--- abdullah/settings.py
+++ abdullah/settings.py
@@ -116,20 +116,25 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#added manually
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
@@ -137,3 +142,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
